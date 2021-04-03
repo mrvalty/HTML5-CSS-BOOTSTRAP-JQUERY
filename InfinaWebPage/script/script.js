9b51dce91fcad2eeee5dd4e440b711cfab99fda0
merve <merve.alty93@gmail.com>
2021-04-03 20:29:53 +0300

    Sıfırdan Web Site

---
+++ InfinaWebPage/script/script.js
@@ -0,0 +1,55 @@
+﻿$(function () {
+    $("#slider1").carousel({
+        interval: 2000,
+        keyword: false,
+        wrap: false
+    });
+});
+
+$(document).ready(function () {
+    $("#myBtn").click(function () {
+        $("#myModal").modal();
+    });
+});
+
+document.getElementsByTagName("a").style.margin = "10px";
+
+$(document).ready(function () {
+    $(".fa").css({
+        "padding": "8px",
+        "font-size": "30px",
+        "width": "32px",
+        "text-aling": "center",
+        "text-decoration": "none",
+        "margin":"5px 2px"
+    });
+
+    $(".fa-facebook-f").css({
+        "background": "#3B5998",
+        "color":"white"
+    });
+
+    $("p#sosyalmedya:a").css({
+        "backgorund":"black"
+    });
+});
+
+
+
+
+
+
+
+//var resimler = [
+//    {
+//        image: 'icindekiler.png'
+//    },
+//    {
+//        image: 'e-defterresim.jpg'
+//    },
+//    {
+//        image: 'e-faturaresim.jpg'
+//    }
+//];
+
+//document.querySelector('.card-img-bottom').setAttribute('src', resimler[2].image);
